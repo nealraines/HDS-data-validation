f643ba7230feccb03c3a70406836080de08b7276
nealraines <nealleoraines@gmail.com>
2025-07-01 15:20:02 -0400

    removed nosetests and added pytest

--- tests/test.py
+++ tests/test.py
@@ -3,21 +3,20 @@
 # Create Date: 7/1/2025
 # Description: Example test file
 
-import nose
+import pytest
 
 def setup_func():
     print("set up test fixtures")
 
+
 def teardown_func():
     print("tear down test fixtures")
 
-@nose.with_setup(setup_func, teardown_func)
-def test():
-    assert False
-
+@pytest.fixture
 def test_evens():
     for i in range(0, 5):
         yield check_even, i, i*3
+
 
 def check_even(n, nn):
     assert n % 2 == 0 or nn % 2 == 0
